39291d7ae51249226ef9d3301d0bfd4cd2fce4f4
Saba Habibi <sabahabibi5993@gmail.com>
2024-12-30 18:53:36 +0330

    Add single product and home page

---
+++ src/pages/product/product.js
@@ -0,0 +1,11 @@
+// افزودن عملکرد کلیک برای نمایش تیک
+// document.querySelectorAll("button").forEach((button) => {
+//   button.addEventListener("click", () => {
+//     const svg = button.querySelector("svg");
+//     const allSvgs = document.querySelectorAll("svg");
+//     allSvgs.forEach((s) => s.classList.add("hidden"));
+//     if (svg) {
+//       svg.classList.toggle("hidden");
+//     }
+//   });
+// });
