18ea9866572e90db922bddde898027a3c779e674
Saba Habibi <sabahabibi5993@gmail.com>
2025-01-02 17:55:04 +0330

    add render produtc home page

--- src/pages/product/product.js
+++ src/pages/product/product.js
@@ -1,11 +1,3 @@
-// افزودن عملکرد کلیک برای نمایش تیک
-// document.querySelectorAll("button").forEach((button) => {
-//   button.addEventListener("click", () => {
-//     const svg = button.querySelector("svg");
-//     const allSvgs = document.querySelectorAll("svg");
-//     allSvgs.forEach((s) => s.classList.add("hidden"));
-//     if (svg) {
-//       svg.classList.toggle("hidden");
-//     }
-//   });
-// });
+const id = location.search;
+console.log(id);
+console.log(location);
